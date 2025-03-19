5dc453a75f9ba328b3d68ccec81554f943ed3da9
Bhawesh Chaudhary <45531771+bhaweshchaudhary@users.noreply.github.com>
2025-03-19 15:15:23 +0545

    init commit

---
+++ src/test/java/com/airbnbclone/airbnbclone/AirbnbcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airbnbclone.airbnbclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
